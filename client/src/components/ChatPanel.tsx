f23197545b14a406a45507866d3cfe9140279d33
MendelCohen770 <mendelecohen770@gmail.com>
2025-04-03 17:06:53 +0300

    ש

--- client/src/components/ChatPanel.tsx
+++ client/src/components/ChatPanel.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ChatPanel = () => {
   return (
-    <div className='bg-slate-300 w-full h-full'>
-      ChatPanel
+    <div className='bg-slate-800 w-full h-full'>
+      <div>
+        {/* כאן יהיה קומפוננטה של הפרופיל של אותו איש קשר שהמשתמש מדבר איתו */}
+      </div>
+      <div>
+    {/* כאן יהיה קומפוננטה של ה chat עצמו עם המקלדת וההודעות. */}
+      </div>
     </div>
   )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,10 +4,10 @@ import ChatPanel from '../components/ChatPanel'
 
 export default function Home() {
   return (
-    <div className="bg-slate-900 h-svh text-gray-500 flex justify-center items-center flex-col">
-      <div className="border-gray-500 border-2 w-full h-full flex relative">
+    <div className="bg-slate-900 h-svh text-gray-500 flex justify-center items-center flex-col p-4">
+      <div className="border-slate-700 border-2 w-full h-full flex relative rounded-xl">
 
-        <div className="absolute top-0 bottom-0 left-1/4 w-0.5 bg-gray-500"></div>
+        <div className="absolute top-0 bottom-0 left-1/4 w-0.5 bg-slate-700"></div>
 
         <div className="w-1/4 flex justify-center items-center p-2">
           <UserPanel/>

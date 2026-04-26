ddf7799774de08f728607a489b58e54ec9f44e19
MendelCohen770 <mendeldinacohen@gmail.com>
2026-04-26 23:35:25 +0300

    feat(client): add ProtectedRoute guard based on user context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
 import { isRtlLanguage } from './i18n';
 
 function App() {
@@ -17,8 +18,22 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Toaster
         position="top-center"
---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useUser } from '../context/useUser';
+
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const userContext = useUser();
+
+  if (!userContext?.user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
